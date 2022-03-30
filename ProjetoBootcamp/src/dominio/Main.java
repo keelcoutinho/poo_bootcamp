0f25358bddec333e667f7f6f2af2ddb0c192078d
Ana Kélvia <99261542+keelcoutinho@users.noreply.github.com>
2022-03-29 21:19:03 -0300

    Commit Projeto Bootcamp

--- ProjetoBootcamp/src/dominio/Main.java
+++ ProjetoBootcamp/src/dominio/Main.java
@@ -12,28 +12,25 @@ public class Main {
 		Curso curso1 = new Curso();
 		
 		curso1.setTitulo("Curso Java");
-		curso1.setDescricao("Aperfeiçoamento em Java, trabalhando com Poo.");
+		curso1.setDescricao("AperfeiÃ§oamento em Java, trabalhando com Poo.");
 		curso1.setCargaHoraria(8);
 		
 		Curso curso2 = new Curso();
 		
 		curso2.setTitulo("Curso Js");
-		curso2.setDescricao("Aperfeiçoamento em Js.");
+		curso2.setDescricao("AperfeiÃ§oamento em Js.");
 		curso2.setCargaHoraria(8);
 						
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("mentoria java");
-		mentoria.setDescricao("Mentoria voltada para conteúdos do curso de Java");
+		mentoria.setDescricao("Mentoria voltada para conteÃºdos do curso de Java");
 		mentoria.setData(LocalDate.now());
 		
 		
-		/*System.out.println("\n "+curso1);
-		System.out.println("\n "+curso2);
-		System.out.println("\n "+mentoria);*/
-		
 		Bootcamp bootcamp = new Bootcamp();
 		bootcamp.setNome("Bootcamp Java Developer");
-		bootcamp.setDescricao("Bootcamp para aperfeiçoamento do seu conhecimento sobre Java");
+		
+		bootcamp.setDescricao("Bootcamp para aperfeiÃ§oamento do seu conhecimento sobre Java");
 		bootcamp.getConteudos().add(curso1);
 		bootcamp.getConteudos().add(curso2);
 		bootcamp.getConteudos().add(mentoria);
@@ -41,14 +38,14 @@ public class Main {
 		Dev devAna = new Dev();
 		devAna.setNome("Ana");
 		devAna.inscreverBootcamp(bootcamp);
-		System.out.println("\n Conteúdos inscritos Ana: "+devAna.getConteudosInscritos());
+		System.out.println("\n ConteÃºdos inscritos Ana: "+devAna.getConteudosInscritos());
 		
 		System.out.println("\n -------------------------------------------------------------");
 		devAna.progredir();	
 		devAna.progredir();	
-		System.out.println("\n Conteúdos concluídos Ana: "+devAna.getConteudosConcluidos());
+		System.out.println("\n ConteÃºdos concluÃ­dos Ana: "+devAna.getConteudosConcluidos());
 		System.out.println("\n -------------------------------------------------------------");
-		System.out.println("\n Conteúdos que ainda faltam ser concluídos Ana: "+devAna.getConteudosInscritos());
+		System.out.println("\n ConteÃºdos que ainda faltam ser concluÃ­dos Ana: "+devAna.getConteudosInscritos());
 		
 		System.err.println("\n __________");
 		System.err.println("\n XP: "+ devAna.calcularTotalXp());
@@ -60,13 +57,13 @@ public class Main {
 		Dev devAhsoka = new Dev();
 		devAhsoka.setNome("Ahsoka");
 		devAhsoka.inscreverBootcamp(bootcamp);
-		System.out.println("\n Conteúdos inscritos Ahsoka: "+devAhsoka.getConteudosInscritos());
+		System.out.println("\n ConteÃºdos inscritos Ahsoka: "+devAhsoka.getConteudosInscritos());
 		
 		System.out.println("\n -------------------------------------------------------------");
 		devAhsoka.progredir();
-		System.out.println("\n Conteúdos concluídos Ahsoka: "+devAna.getConteudosConcluidos());
+		System.out.println("\n ConteÃºdos concluÃ­dos Ahsoka: "+devAna.getConteudosConcluidos());
 		System.out.println("\n -------------------------------------------------------------");
-		System.out.println("\n Conteúdos que ainda faltam ser concluídos Ahsoka: "+devAhsoka.getConteudosInscritos());
+		System.out.println("\n ConteÃºdos que ainda faltam ser concluÃ­dos Ahsoka: "+devAhsoka.getConteudosInscritos());
 		
 		System.err.println("\n __________");
 		System.err.println("\n XP: "+ devAhsoka.calcularTotalXp());
